3a39525b2a1aadf09ac2fa3ff8c79e7227b8862a
nitin kalmaste <nk@nitins-MacBook-Pro.local>
2025-11-09 09:04:30 +0530

    updated grep lite

--- grep_lite/src/main.rs
+++ grep_lite/src/main.rs
@@ -1,37 +1,54 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use clap::{Arg, Command};
 use regex::Regex;
-use clap::{Arg,Command};
 
 fn main() {
-
     let args = Command::new("grep-lite")
-    .version("0.1.1")
-    .about("search pattern in text")
-    .arg(
-        Arg::new("pattern")
-        .help("the pattern to search for")
-        .required(true)
-        .value_name("REGEX"),
-    )
-    .get_matches();
+        .version("0.1.1")
+        .about("search pattern in text")
+        .arg(
+            Arg::new("pattern")
+                .help("the pattern to search for")
+                .required(true)
+                .value_name("REGEX"),
+        )
+        .arg(
+            Arg::new("input")
+                .help("from file to search for")
+                .value_name("file")
+                .required(true),
+        )
+        .get_matches();
 
-    
-    let pattern = args.get_one::<String>("pattern")
-        .expect("required by clap");
-    
+    let pattern = args.get_one::<String>("pattern").expect("required by clap");
     let re = Regex::new(pattern).unwrap();
 
-    let quote = "\
-    Every face, every shop, bedroom window, public house, and dark square,
-    is a picture feverishly turned--in search of what?
-    It is the same with books.
+    let input = args.get_one::<String>("input").expect("required by clap");
 
-    What do we seek through millions of pages?";
-    
-    for (idx,line) in quote.lines().enumerate(){
-        let contains_substring = re.find(line); 
-        match contains_substring {
-            Some(_) => println!("{}:{}",idx,line),
-            None => (),
+    let file = match File::open(input) {
+        Ok(f) => f,
+        Err(_e) => {
+            eprintln!("provide full file path:");
+            return;
         }
     };
+
+    let reader = BufReader::new(file);
+
+    process_file(reader, re);
+}
+
+fn process_file<T: BufRead + Sized>(reader: T, re: Regex) {
+    for (idx, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        let contains_substring = re.find(&line);
+        match contains_substring {
+            Some(_) => println!("{} : {}", idx, line),
+            None => (),
+        }
+    }
 }
